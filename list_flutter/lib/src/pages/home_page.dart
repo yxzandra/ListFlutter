7cc6ab0fc755f82f3c836c6ac02ff8d9d43c7aac
Yxzandra Cordero <yxzandra.cordero@tinet.cl>
2021-02-25 11:33:05 -0300

    - Se agrega la vista de detalle - Se redirecciona a la vista de detalle al presionar algun item de la lista

--- list_flutter/lib/src/pages/home_page.dart
+++ list_flutter/lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:list_flutter/src/pages/alert_page.dart';
 import 'package:list_flutter/src/providers/menu_provider.dart';
 import 'package:list_flutter/src/utils/icono_string_util.dart';
 
@@ -19,20 +20,23 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listItems(snapshot.data),
+          children: _listItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listItems(List<dynamic> data) {
+  List<Widget> _listItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
     data.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });
